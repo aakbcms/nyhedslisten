24894e2ce8ccb5a2f7d553451548bee11c91ffc2
Ture Gjørup <ture@ignatz.dk>
2021-02-08 14:31:46 +0100

    AAKBET-727: Fixed typos

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -45,9 +45,9 @@ class FeedController extends AbstractController
 
         $categories = $this->entityManager->getRepository(Category::class)->findByMaterialDate($date);
 
-        // HeyLoyalty doesn't support sorting on multiple values, like SQL does
+        // HeyLoyalty doesn't support sorting on multiple values like SQL does
         // ( E.g "ORDER BY author ASC, year DESC"). HeyLoyalty only allows sorting on one
-        // key in the json feed. "Sortkey" is used the HeyLoyalty setup to force HeyLoyalty
+        // key in the json feed. "sortkey" is used in the HeyLoyalty setup to force HeyLoyalty
         // to maintain the order the materiels have in the feed.
         // However HeyLoyalty does "text" sort, not "numeric" sort, so "15" comes after "149"
         // in their sorting. To guard against this we start the sortKey counter at 1000000
